4dcade4d0293933b2b578e2c9022ec4b52108c76
coding67-KR <a51286652@gmail.com>
2026-09-06 09:08:47 +0900

    Fix AGP 9 built-in Kotlin configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -20,8 +19,4 @@ android {
             isMinifyEnabled = false
         }
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
